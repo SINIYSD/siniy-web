8f5bdaa493772ee19ef1e285c2793cf2b98e4d5c
SINIYSD <siniysolucionesintegrales@gmail.com>
2026-02-17 18:41:01 -0500

    Update components.js

--- components.js
+++ components.js
@@ -103,7 +103,7 @@ class SiniyFooter extends HTMLElement {
                     
                     
 
-                    <img src="https://epayco.com/wp-content/uploads/2023/01/epayco_pago_seguro_gray.png" alt="Pagos con ePayco" style="width: 100%; max-width: 260px;">
+                    <img src="epayco-pasarela.png" alt="Pagos con ePayco" style="width: 100%; max-width: 260px;">
                 </div>
 
             </div>
